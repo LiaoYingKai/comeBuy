8642362fc3cbf19de466733da00b2fe918fe7bd6
Kai <b10223052@yuntech.org.tw>
2019-02-09 16:03:30 +0800

    main page nav style is ok

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import './registerServiceWorker'
 import VueCookies from 'vue-cookies'
 Vue.use(VueCookies)
-
+Vue.use(ElementUI);
 Vue.config.productionTip = false
-
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+  })
+  .$mount('#app')
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,16 +1,71 @@
 <template>
 <div class="mainPage">
-  <nav>
-    test
-  </nav>
+  <el-row :gutter="20" class="nav">
+    <el-col :span="4">
+      <img src="../assets/logo.jpg" alt="">
+    </el-col>
+    <el-col :span="14">
+      <div class="">
+        <el-menu mode="horizontal" :router="false">
+          <el-submenu index="1">
+            <template slot="title">買家</template>
+            <el-menu-item index="2-1">觀看直播</el-menu-item>
+            <el-menu-item index="2-2">更新個人資訊</el-menu-item>
+            <el-menu-item index="2-3">查看訂單</el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">賣家</template>
+            <el-menu-item index="#">開始直播</el-menu-item>
+            <el-menu-item index="#">商品資訊</el-menu-item>
+            <el-menu-item index="#">管理訂單</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </div>
+    </el-col>
+    <el-col :span="2"><img :src="sticker" alt=""></el-col>
+    <el-col :span="2">
+      <div class="">{{name}}</div>
+    </el-col>
+    <el-col :span="2">登出</el-col>
+  </el-row>
+
   <router-view></router-view>
 </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    sticker: function() {
+      return this.$store.getters.userInfo.avatar
+    },
+    name: function() {
+      return this.$store.getters.userInfo.name
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.nav {
+  height: 60px;
+    display: flex;
+    background-color: #fff;
+    img {
+        height: 60px;
+    }
+    .el-col-2 {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+}
+.el-menu.el-menu--horizontal{
+  border:0px;
+}
 </style>
